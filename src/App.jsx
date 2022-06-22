9ef0735ed4fe998fdad52f2b5cee8bf8353d8350
Nils.Rausch <Nils.Rausch@exxeta.com>
2022-06-22 11:13:35 +0200

    Styling Navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import Dashboard from "./components/Dashboard";
+import Chat from "./components/chat/Chat";
+import Journal from "./components/journal/Journal";
+import Account from "./components/account/Account";
+import NotFound from "./components/NotFound";
+import styles from "./App.module.css";
 
 function App() {
   return (
     <>
-      <Navigation />
+      <div className={styles.app}>
+        <Navigation />
 
-      <Routes>
-        <Route index element={<Dashboard />} />
-        <Route path="dashboard" element={<Dashboard />} />
-      </Routes>
+        <Routes>
+          <Route index element={<Dashboard />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="chat" element={<Chat />} />
+          <Route path="journal" element={<Journal />} />
+          <Route path="account" element={<Account />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
     </>
   );
 }
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import "./NavigationStyle.css";
 
 function Navigation() {
   return (
     <>
-      <NavLink to="/"></NavLink>
-      <NavLink to="/dashboard">Dashboard</NavLink>
+      <div className="topnav">
+        <div style={{ display: "inline-block", float: "left", marginTop: 13, marginLeft: 15, marginRight: 15, color: "#fff" }}>
+          Focus Positive
+        </div>
+        <NavLink style={{ display: "inline-block" }} to="/dashboard">
+          Dashboard
+        </NavLink>
+        <NavLink style={{ display: "inline-block" }} to="/chat">
+          Random Chat
+        </NavLink>
+        <NavLink style={{ display: "inline-block" }} to="/journal">
+          Journal
+        </NavLink>
+        <NavLink style={{ display: "inline-block" }} to="/account">
+          Account
+        </NavLink>
+      </div>
     </>
   );
 }
