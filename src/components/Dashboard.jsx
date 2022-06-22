5066cd4bfc604876aea461cb4f9ae7acf9ce4eae
Nils.Rausch <Nils.Rausch@exxeta.com>
2022-06-22 14:35:10 +0200

    Background Img On Dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,11 +1,19 @@
 import React from "react";
-
+import background from "../img/Munnar4.jpg";
+import "./DashboardStyle.css";
 
 function Dashboard() {
   const user = JSON.parse(localStorage.getItem("user"));
- 
 
-  return <div>Wilkommen {user.fname}</div>;
-} 
+  return (
+    <div className="dashboard">
+      <div className="bg">
+        <div style={{ backgroundColor: "#fff", padding: "10px" }}>
+          Wilkommen {user.fname} {user.lname}
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Dashboard;
