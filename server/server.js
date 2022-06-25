d0df3b5433cb6e3733bf44a4a33857ea32b977f9
Nils.Rausch <Nils.Rausch@exxeta.com>
2022-06-25 10:22:57 +0200

    Register Page Form start

--- server/server.js
+++ server/server.js
@@ -50,7 +50,7 @@ server.post("/api/auth/register", (req, res) => {
 
     let last_item_id = data.users[data.users.length - 1].id;
 
-    data.users.push({ id: last_item_id + 1, email: email, password: password });
+    data.users.push({ id: last_item_id + 1, email: email, password: password});
     let writeData = fs.writeFile(
       "./users.json",
       JSON.stringify(data),
