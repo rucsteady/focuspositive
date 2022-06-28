7cd14b6c355dcb5ad063c38205e40194c0257529
Nils.Rausch <Nils.Rausch@exxeta.com>
2022-06-28 08:17:24 +0200

    Auth Works with register

--- src/components/account/Account.jsx
+++ src/components/account/Account.jsx
@@ -1,27 +1,39 @@
 import React, { useContext } from "react";
 import AuthContext from "../../context/AuthContext";
 import Button from "@mui/material/Button";
+import "./AccountStyle.css";
 
 function Account() {
   const { users, handleLogOut, currentEmail } = useContext(AuthContext);
 
   return (
     <>
-      <div>Account</div>
-      <div>Eingeloggt als: {users
-            .filter((user) => user.email === currentEmail)
-            .map((user) => user.firstname)}    
-            
+      <div className="account">
+        <div>Account</div>
+        <div>
+          Eingeloggt als
+          <p style={{ margin: 5 }}>
+            Vorname: 
             {users
-            .filter((user) => user.email === currentEmail)
-            .map((user) => user.lastname)}</div>
-
-      <div>
+              .filter((user) => user.email === currentEmail)
+              .map((user) => user.firstname)}
+          </p>
+          <p style={{ margin: 5 }}>
+            Nachname: 
+            {users
+              .filter((user) => user.email === currentEmail)
+              .map((user) => user.lastname)}
+          </p>
+        </div>
+        <div>
         <Button variant="contained" onClick={handleLogOut}>
           {" "}
           Logout
         </Button>{" "}
       </div>
+      </div>
+
+    
     </>
   );
 }
