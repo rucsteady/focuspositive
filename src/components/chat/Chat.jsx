7a892c99c7fb0772874405c8dd9a4e1512181d80
Nils.Rausch <Nils.Rausch@exxeta.com>
2022-07-14 10:13:07 +0200

    Adding Random Chat Content stubs

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,5 +1,9 @@
-import React, { useContext } from "react";
+import { Toolbar, Typography } from "@mui/material";
+import { Box, margin } from "@mui/system";
+import React, { Fragment, useContext } from "react";
 import AuthContext from "../../context/AuthContext";
+import Bar from "./Bar";
+import RandomChat from "./RandomChat";
 
 function Chat() {
   const { users, currentUser, currentEmail } = useContext(AuthContext);
@@ -7,14 +11,17 @@ function Chat() {
   console.log("currentUser:", currentUser[0].firstname);
 
   return (
-    <div>
-      Login in user:{currentUser[0].firstname}
-      {users
-        .filter((user) => user.email === currentEmail)
-        .map((user) => user.firstname)}
-
-        <p>{currentEmail}</p>
-    </div>
+    <Fragment>
+      <Toolbar>
+        <Box mr={2}>
+          <Typography variant="h6">
+            {currentUser[0].firstname} Random Chat
+          </Typography>
+        </Box>
+      </Toolbar>
+      <RandomChat />
+      <Bar />
+    </Fragment>
   );
 }
 
---
+++ src/components/chat/RandomChat.jsx
@@ -0,0 +1,14 @@
+import { Container, Paper } from "@mui/material";
+import React, { Fragment } from "react";
+
+function RandomChat() {
+  return (
+    <Fragment>
+      <Container>
+        <Paper elevation={5}></Paper>
+      </Container>
+    </Fragment>
+  );
+}
+
+export default RandomChat;
