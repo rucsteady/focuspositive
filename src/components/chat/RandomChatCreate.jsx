02266f6d971c97ac987e7f48c458cd5881022452
rucsteady <rausch3n@gmail.com>
2022-07-24 17:53:07 +0200

    Random Chat Create works

--- src/components/chat/RandomChatCreate.jsx
+++ src/components/chat/RandomChatCreate.jsx
@@ -62,88 +62,68 @@ function RandomChatCreate({ handleShowChatInfo }) {
 
   return (
     <Fragment>
-      <Container>
-        <Paper
-          elevation={0}
-          sx={{
-            width: 350,
-            padding: 4,
-          }}
-        >
-          <Typography variant='h6'>Erstelle einen neuen Random Chat</Typography>
-          <Grid container>
-            <Grid item mt={4} maxWidth={'350px'}>
-              <Typography>
-                Hier kannst du einen eigenen Random Chat erstellen. Gib deinem
-                Chat einen Namen. Wenn du magst, kannst du Themen eintragen,
-                über die du gerne sprechen möchtest.
-              </Typography>
-            </Grid>
-            <Box
-              component='form'
-              noValidate
-              onSubmit={handleSubmit}
-              sx={{ mt: 3 }}
-            >
-              <Grid container spacing={2}>
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    id='name'
-                    label='Random Chat Name'
-                    name='name'
-                    autoFocus
-                    value={name}
-                    onChange={(e) => {
-                      setName(e.target.value);
-                    }}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    name='topic'
-                    label='Gesprächsthemen'
-                    id='topic'
-                    value={topic}
-                    onChange={(e) => {
-                      setTopic(e.target.value);
-                    }}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <LocalizationProvider dateAdapter={AdapterMoment}>
-                    <DateTimePicker
-                      okLabel='Text'
-                      clearLabel='Text'
-                      cancelLabel='Text'
-                      label='Datum'
-                      value={date}
-                      onChange={(newDate) => {
-                        setDate(newDate);
-                      }}
-                      renderInput={(params) => <TextField {...params} />}
-                    />
-                  </LocalizationProvider>
-                </Grid>
-              </Grid>
-              <Button
-                type='submit'
+      <Typography variant='h6'>Erstelle einen neuen Random Chat</Typography>
+      <Grid container>
+        <Grid item mt={4}>
+          <Typography>
+            Hier kannst du einen eigenen Random Chat erstellen. Gib deinem Chat
+            einen Namen. Wenn du magst, kannst du Themen eintragen, über die du
+            gerne sprechen möchtest.
+          </Typography>
+        </Grid>
+        <Box component='form' noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                required
                 fullWidth
-                variant='contained'
-                sx={{ mt: 3, mb: 2 }}
-              >
-                erstellen
-              </Button>
-            </Box>
-            <Button size='small' variant='text' onClick={handleShowChatInfo}>
-              Zurück
-            </Button>
+                id='name'
+                label='Random Chat Name'
+                name='name'
+                autoFocus
+                value={name}
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                name='topic'
+                label='Gesprächsthemen'
+                id='topic'
+                value={topic}
+                onChange={(e) => {
+                  setTopic(e.target.value);
+                }}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <LocalizationProvider dateAdapter={AdapterMoment}>
+                <DateTimePicker
+                  okLabel='Text'
+                  clearLabel='Text'
+                  cancelLabel='Text'
+                  label='Datum'
+                  value={date}
+                  onChange={(newDate) => {
+                    setDate(newDate);
+                  }}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+              </LocalizationProvider>
+            </Grid>
           </Grid>
-        </Paper>
-      </Container>
+          <Button type='submit' variant='contained' sx={{ mt: 3, mb: 2 }}>
+            erstellen
+          </Button>
+        </Box>
+        <Button size='small' variant='text' onClick={handleShowChatInfo}>
+          Zurück
+        </Button>
+      </Grid>
     </Fragment>
   );
 }
