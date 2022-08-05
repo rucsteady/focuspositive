e9cc5c8a2e93168acd4377823e550be859d5081f
Nils.Rausch <Nils.Rausch@exxeta.com>
2022-08-05 12:02:24 +0200

    Working on Password Edit Account

--- src/components/account/Account.jsx
+++ src/components/account/Account.jsx
@@ -1,7 +1,13 @@
 import React, { useContext, useState } from "react";
 import AuthContext from "../../context/AuthContext";
 import Button from "@mui/material/Button";
-import { Container, Divider, Paper, Typography } from "@mui/material";
+import {
+  Container,
+  Divider,
+  Paper,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { Box } from "@mui/system";
 
 function Account() {
@@ -15,6 +21,60 @@ function Account() {
           <Box elevation={0}>
             <Typography variant="h5">Account</Typography>
             <Divider sx={{ padding: 1 }} />
+
+            <Box>
+              <Typography sx={{ padding: 1 }}>Account bearbeiten:</Typography>
+              <TextField
+                sx={{ padding: 1 }}
+                defaultValue={users
+                  .filter((user) => user.email === currentEmail)
+                  .map((user) => user.firstname)}
+                label="Vorname"
+              >
+                Vorname:{` `}
+                {users
+                  .filter((user) => user.email === currentEmail)
+                  .map((user) => user.firstname)}
+              </TextField>
+              <TextField sx={{ padding: 1 }} label="Nachname">
+                Nachname:{` `}
+                {users
+                  .filter((user) => user.email === currentEmail)
+                  .map((user) => user.lastname)}
+              </TextField>
+              <TextField sx={{ padding: 1 }}>
+                E-Mail:{` `}
+                {currentEmail}
+              </TextField>
+              <FormControl sx={{ m: 1, width: "25ch" }} variant="outlined">
+                <InputLabel htmlFor="outlined-adornment-password">
+                  Password
+                </InputLabel>
+                <OutlinedInput
+                  id="outlined-adornment-password"
+                  type={values.showPassword ? "text" : "password"}
+                  value={values.password}
+                  onChange={handleChange("password")}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        edge="end"
+                      >
+                        {values.showPassword ? (
+                          <VisibilityOff />
+                        ) : (
+                          <Visibility />
+                        )}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                  label="Password"
+                />
+              </FormControl>
+            </Box>
 
             <Box>
               <Typography sx={{ padding: 1 }}>Eingeloggt als:</Typography>
@@ -35,6 +95,7 @@ function Account() {
                 {currentEmail}
               </Typography>
             </Box>
+
             <div style={{ marginTop: 10 }}>
               <Button
                 variant="contained"
